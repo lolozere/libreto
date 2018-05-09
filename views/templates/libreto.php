9e676e34a737cf19a4d448bb5b04bc63dcbad750
Laurent Chedanne <laurent@chedanne.pro>
2018-05-09 16:51:01 +0200

    Send http header redirect before output

--- views/templates/libreto.php
+++ views/templates/libreto.php
@@ -1,17 +1,19 @@
-<?php global $libreto ?>
-<?php snippet('header') ?>
+<?php
+global $libreto;
+if (!$libreto->pads()->selected()):
+    header('Location: ?mode=write');
+    return;
+endif;
+
+snippet('header'); ?>
 <div id="container">
-  <?php snippet('introduction') ?>
-  <?php snippet('nav') ?>
-  <article>
-    <?php
-    if ($libreto->pads()->selected()) :
-      $iframe = $libreto->pads()->selected()->url();
-    else :
-      header('Location: ?mode=write');
-    endif;
-    ?>
-    <iframe id="framepad" name="myframe" width=600 height=400 src="<?= $iframe ?>"></iframe>
-  </article>
+    <?php snippet('introduction') ?>
+    <?php snippet('nav') ?>
+    <article>
+        <?php
+        $iframe = $libreto->pads()->selected()->url();
+        ?>
+        <iframe id="framepad" name="myframe" width=600 height=400 src="<?= $iframe ?>"></iframe>
+    </article>
 </div>
 <?php snippet('footer') ?>
