21b6bf3a8974b664c7aa12d94e25de6deb97aeaa
Laurent Chedanne <laurent@chedanne.pro>
2018-05-11 11:29:26 +0200

    No frame for reader

--- views/templates/libreto.php
+++ views/templates/libreto.php
@@ -12,8 +12,14 @@ snippet('header'); ?>
     <article>
         <?php
         $iframe = $libreto->pads()->selected()->url();
-        ?>
+        if ($_SESSION['mode'] == 'write' || $iframe[0] != '/'):?>
         <iframe id="framepad" name="myframe" width=600 height=400 src="<?= $iframe ?>"></iframe>
+        <?php else:
+          $scheme = ($_SERVER['REMOTE_PORT'] == 443)?'https':'http';?>
+        <section class="reader-pane">
+          <?php echo  file_get_contents($scheme . '://' . $_SERVER['HTTP_HOST'] . $iframe);?>
+        </section>
+        <?php endif;?>
     </article>
 </div>
 <?php snippet('footer') ?>
--- views/templates/reader.php
+++ views/templates/reader.php
@@ -1,36 +1,66 @@
+<style>
+  main.content {
+    padding: 1rem;
+    max-width: 40rem;
+    font-size: 1rem;
+  }
+
+  main.content p,
+  main.content h1,
+  main.content h2,
+  main.content h3,
+  main.content h4,
+  main.content h5,
+  main.content h6,
+  main.content ul,
+  main.content blockquote {
+    margin: 0;
+    margin-bottom: 1rem;
+  }
+  main.content hr {
+    border: none;
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 1rem;
+  }
+  main.content img {
+    max-width: 100%;
+    height: auto;
+    margin: auto;
+    display: block;
+  }
+  main.content a {
+    color: blue; text-decoration: underline;
+  }
+  main.content h1 {
+    font-size: 1.8rem;
+  }
+</style>
 <?php
 global $libreto;
-?>
-<!doctype html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <title><?= $libreto->name() ?> - Libreto</title>
-  <link rel="stylesheet" href="/libreto/assets/style-reader.css">
-  <style>
-    <?= $libreto->pads()->css() ?>
-  </style>
-</head>
-<body>
-  <main class="content">
-    <?php
-    if ($libreto->router()->pad()) :
-      ?>
-      <?= $libreto->pads()->selected()->html() ?>
-      <?php
-    else:
-      ?>
-      <h1><?= $this->name() ?></h1>
-      <?php
-      $pads = $this->pads()->children('visible');
-      foreach($pads as $pad) :
-        ?>
-        <h2><?= $pad->name() ?></h2>
-        <div><?= $pad->html() ?></div>
-        <?php
-      endforeach;
-    endif;
+
+$css = $libreto->pads()->css();
+if ($css !== false) {
+  require ROOT . DS . "libreto" . DS . "vendor" . DS . "lessphp" . DS . "lessc.inc.php";
+  $less = new lessc;
+  echo '<style>' . $less->compile("main.content { $css }") . '</style>';
+}?>
+<main class="content">
+  <?php
+  if ($libreto->router()->pad()) :
     ?>
-  </main>
-</body>
-</html>
+    <?= $libreto->pads()->selected()->html() ?>
+    <?php
+  else:
+    ?>
+    <h1><?= $this->name() ?></h1>
+    <?php
+    $pads = $this->pads()->children('visible');
+    foreach($pads as $pad) :
+      ?>
+      <h2><?= $pad->name() ?></h2>
+      <div><?= $pad->html() ?></div>
+      <?php
+    endforeach;
+  endif;
+  ?>
+</main>
